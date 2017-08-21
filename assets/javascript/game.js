96224d7882cb60ca27ea8819996b5e4e8cc6f3b8
Brian Patrick <batpat772@gmail.com>
2017-08-20 21:07:32 -0700

    added html

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+// I need 4 crystals buttons to display on the page
+//At the start of the game the player will be given a random number to match in order to win
+    //A new random number must be generated at the start of every new game
+//When the player clicks the crystal, it will add specific amount of points to their total.
+    //The value of each crystal is hidden from the player
+    //The total score will be displayed
+    //Crystal point value changes at the start of each new game
+//Clicking on a crystal will update the player's score
+//The player wins if they match the randomly generated number
+//The player loses if they go over the randomly generated number
+//The game resets when the player wins or loses
+//The game must display total wins and loses
+    //!!IMPORTANT!! The game must reset itself. You cannot refresh the page
+        //to restart the game
+//
+
+
+
+
+
